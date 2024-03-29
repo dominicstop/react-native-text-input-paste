ce2ec919345a2a83e6f5766c47b97f70667f1f09
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-03-29 17:35:43 +0800

    ⭐️ Impl: Types - Add `MimeType`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from "./components/TextInputPaste";
+
 export * from "./types/UniformTypeIdentifier";
 export * from "./types/EditMenuDefaultActions";
 export * from "./types/HelperTypes";
+export * from "./types/MimeType";
 
 export * as RNTextInputWrapperLibEnv from './constants/LibEnv';
